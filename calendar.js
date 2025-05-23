f253d28ca3052244993a4059fc04a53377aa50aa
seba06200 <robertseeba@gmail.com>
2025-05-23 13:20:43 +0200

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,30 +1,296 @@
 const calendar = [
   {
-    day: 1,
-    matches: [
-      { home: "Les Charognards", away: "KFC" },
-      { home: "Les BrozArtiste", away: "Rocher phocéen" },
-      { home: "Lyonissa", away: "PastaChorizo FC" },
-      { home: "Crazy Dogs Bikers", away: "FC District" }
+    "Jour": 1,
+    "matches": [
+      {
+        "home": "Les Charognards",
+        "away": "KFC"
+      },
+      {
+        "home": "Les BrozArtiste",
+        "away": "Rocher phocéen"
+      },
+      {
+        "home": "Lyonissa",
+        "away": "PastaChorizo FC"
+      },
+      {
+        "home": "Crazy Dogs Bikers",
+        "away": "FC District"
+      }
     ]
   },
   {
-    day: 2,
-    matches: [
-      { home: "Les Charognards", away: "Rocher phocéen" },
-      { home: "KFC", away: "PastaChorizo FC" },
-      { home: "Les BrozArtiste", away: "FC District" },
-      { home: "Lyonissa", away: "Crazy Dogs Bikers" }
+    "Jour": 2,
+    "matches": [
+      {
+        "home": "Les Charognards",
+        "away": "Rocher phocéen"
+      },
+      {
+        "home": "KFC",
+        "away": "PastaChorizo FC"
+      },
+      {
+        "home": "Les BrozArtiste",
+        "away": "FC District"
+      },
+      {
+        "home": "Lyonissa",
+        "away": "Crazy Dogs Bikers"
+      }
     ]
   },
   {
-    day: 3,
-    matches: [
-      { home: "Les Charognards", away: "PastaChorizo FC" },
-      { home: "Rocher phocéen", away: "FC District" },
-      { home: "KFC", away: "Crazy Dogs Bikers" },
-      { home: "Les BrozArtiste", away: "Lyonissa" }
+    "Jour": 3,
+    "matches": [
+      {
+        "home": "Les Charognards",
+        "away": "PastaChorizo FC"
+      },
+      {
+        "home": "Rocher phocéen",
+        "away": "FC District"
+      },
+      {
+        "home": "KFC",
+        "away": "Crazy Dogs Bikers"
+      },
+      {
+        "home": "Les BrozArtiste",
+        "away": "Lyonissa"
+      }
     ]
   },
-  // … (Tu peux ajouter les journées 4 à 14 ou me demander de t’envoyer un fichier complet prêt)
+  {
+    "Jour": 4,
+    "matches": [
+      {
+        "home": "Les Charognards",
+        "away": "FC District"
+      },
+      {
+        "home": "PastaChorizo FC",
+        "away": "Crazy Dogs Bikers"
+      },
+      {
+        "home": "Rocher phocéen",
+        "away": "Lyonissa"
+      },
+      {
+        "home": "KFC",
+        "away": "Les BrozArtiste"
+      }
+    ]
+  },
+  {
+    "Jour": 5,
+    "matches": [
+      {
+        "home": "Les Charognards",
+        "away": "Crazy Dogs Bikers"
+      },
+      {
+        "home": "FC District",
+        "away": "Lyonissa"
+      },
+      {
+        "home": "PastaChorizo FC",
+        "away": "Les BrozArtiste"
+      },
+      {
+        "home": "Rocher phocéen",
+        "away": "KFC"
+      }
+    ]
+  },
+  {
+    "Jour": 6,
+    "matches": [
+      {
+        "home": "Les Charognards",
+        "away": "Lyonissa"
+      },
+      {
+        "home": "Crazy Dogs Bikers",
+        "away": "Les BrozArtiste"
+      },
+      {
+        "home": "FC District",
+        "away": "KFC"
+      },
+      {
+        "home": "PastaChorizo FC",
+        "away": "Rocher phocéen"
+      }
+    ]
+  },
+  {
+    "Jour": 7,
+    "matches": [
+      {
+        "home": "Les Charognards",
+        "away": "Les BrozArtiste"
+      },
+      {
+        "home": "Lyonissa",
+        "away": "KFC"
+      },
+      {
+        "home": "Crazy Dogs Bikers",
+        "away": "Rocher phocéen"
+      },
+      {
+        "home": "FC District",
+        "away": "PastaChorizo FC"
+      }
+    ]
+  },
+  {
+    "Jour": 8,
+    "matches": [
+      {
+        "home": "KFC",
+        "away": "Les Charognards"
+      },
+      {
+        "home": "Rocher phocéen",
+        "away": "Les BrozArtiste"
+      },
+      {
+        "home": "PastaChorizo FC",
+        "away": "Lyonissa"
+      },
+      {
+        "home": "FC District",
+        "away": "Crazy Dogs Bikers"
+      }
+    ]
+  },
+  {
+    "Jour": 9,
+    "matches": [
+      {
+        "home": "Rocher phocéen",
+        "away": "Les Charognards"
+      },
+      {
+        "home": "PastaChorizo FC",
+        "away": "KFC"
+      },
+      {
+        "home": "FC District",
+        "away": "Les BrozArtiste"
+      },
+      {
+        "home": "Crazy Dogs Bikers",
+        "away": "Lyonissa"
+      }
+    ]
+  },
+  {
+    "Jour": 10,
+    "matches": [
+      {
+        "home": "PastaChorizo FC",
+        "away": "Les Charognards"
+      },
+      {
+        "home": "FC District",
+        "away": "Rocher phocéen"
+      },
+      {
+        "home": "Crazy Dogs Bikers",
+        "away": "KFC"
+      },
+      {
+        "home": "Lyonissa",
+        "away": "Les BrozArtiste"
+      }
+    ]
+  },
+  {
+    "Jour": 11,
+    "matches": [
+      {
+        "home": "FC District",
+        "away": "Les Charognards"
+      },
+      {
+        "home": "Crazy Dogs Bikers",
+        "away": "PastaChorizo FC"
+      },
+      {
+        "home": "Lyonissa",
+        "away": "Rocher phocéen"
+      },
+      {
+        "home": "Les BrozArtiste",
+        "away": "KFC"
+      }
+    ]
+  },
+  {
+    "Jour": 12,
+    "matches": [
+      {
+        "home": "Crazy Dogs Bikers",
+        "away": "Les Charognards"
+      },
+      {
+        "home": "Lyonissa",
+        "away": "FC District"
+      },
+      {
+        "home": "Les BrozArtiste",
+        "away": "PastaChorizo FC"
+      },
+      {
+        "home": "KFC",
+        "away": "Rocher phocéen"
+      }
+    ]
+  },
+  {
+    "Jour": 13,
+    "matches": [
+      {
+        "home": "Lyonissa",
+        "away": "Les Charognards"
+      },
+      {
+        "home": "Les BrozArtiste",
+        "away": "Crazy Dogs Bikers"
+      },
+      {
+        "home": "KFC",
+        "away": "FC District"
+      },
+      {
+        "home": "Rocher phocéen",
+        "away": "PastaChorizo FC"
+      }
+    ]
+  },
+  {
+    "Jour": 14,
+    "matches": [
+      {
+        "home": "Les BrozArtiste",
+        "away": "Les Charognards"
+      },
+      {
+        "home": "KFC",
+        "away": "Lyonissa"
+      },
+      {
+        "home": "Rocher phocéen",
+        "away": "Crazy Dogs Bikers"
+      },
+      {
+        "home": "PastaChorizo FC",
+        "away": "FC District"
+      }
+    ]
+  }
 ];
