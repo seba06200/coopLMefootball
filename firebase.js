626670b3c23c183eedb9c4948f06cc167b7a03ad
seba06200 <robertseeba@gmail.com>
2025-05-23 18:52:10 +0200

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9i9jx7Wz1RBYx8MziXOdfVtu7Si64njvo",
+  authDomain: "competition-foot.firebaseapp.com",
+  projectId: "competition-foot",
+  storageBucket: "competition-foot.appspot.com",
+  messagingSenderId: "731546807160",
+  appId: "1:731546807160:web:829b3101ee86d9a83bf68d"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+window.db = db;
